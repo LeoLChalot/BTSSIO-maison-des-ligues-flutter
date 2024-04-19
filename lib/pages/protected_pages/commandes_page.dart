46a59fd14fc6529347806731096c91210ab02c98
LeoLChalot <77191750+LeoLChalot@users.noreply.github.com>
2024-04-19 15:30:54 +0200

    Add commandes_page.dart

---
+++ lib/pages/protected_pages/commandes_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CommandesPage extends StatefulWidget {
+  const CommandesPage({super.key});
+
+  @override
+  State<CommandesPage> createState() => _CommandesPageState();
+}
+
+class _CommandesPageState extends State<CommandesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
